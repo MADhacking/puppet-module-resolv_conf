42e58dd2aacb8fbbd3ce714f0d966ecbd38b797f
Max Hacking <max@hacking.co.uk>
2015-05-03 22:35:17 +0100

    chg: dev: Common options are now named parameters. chg: dev: Updated README.md.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,6 @@
-class resolv_conf ($domain, $nameservers, $options = undef) {
+# resolv_conf is used to manage the /etc/resolv.conf file.
+
+class resolv_conf ($domain, $nameservers, $timeout = 2, $attempts = undef, $rotate = true, $options = undef) {
   file { 'resolv.conf':
     ensure  => file,
     path    => '/etc/resolv.conf',
